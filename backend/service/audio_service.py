e77fd030d8279ac351f6c409eaaa9a6fb5f925b4
Lipun Dani <lipundani@gmail.com>
2026-03-17 21:23:02 +0530

    feat: added audio feature extraction logic from audio

--- backend/service/audio_service.py
+++ backend/service/audio_service.py
@@ -1,7 +1,8 @@
 import ffmpeg
+import librosa
 from pathlib import Path
 
-import librosa
+import numpy as np
 
 from backend.config import audio_store_dir
 
@@ -52,4 +53,46 @@ def split_audio(audio_path, segment_duration = 3):
 
     except Exception as ex:
         print('Unexpected Error while splitting audio', ex)
+        raise
+
+# extract mfcc, delta, delta2 per segment
+def extract_audio_features(video_path):
+    try:
+        audio_path = extract_audio(video_path)
+        segments, sr = split_audio(audio_path)
+
+        if not segments:
+            return ValueError('No audio segments found')
+
+        audio_features = []
+
+        for segment in segments:
+            # mfcc = (19, time_frames)
+            mfcc = librosa.feature.mfcc(y = segment, sr = sr, n_mfcc = 19)
+
+            # delta = rate of change
+            delta = librosa.feature.delta(mfcc)
+
+            # delta2 = acceleration
+            delta2 = librosa.feature.delta(mfcc, order=2)
+
+            # mean across time = (19,) each
+            mfcc_mean = np.mean(mfcc.T, axis=0)
+            delta_mean = np.mean(delta.T, axis=0)
+            delta2_mean = np.mean(delta2.T, axis=0)
+
+            # combine → (57,)
+            feature_vector = np.hstack([mfcc_mean, delta_mean, delta2_mean])
+
+            segments.append(feature_vector)
+
+        audio_features = np.array(audio_features)
+        print('audio features shape: ', audio_features.shape)
+
+        return audio_features
+
+    except ValueError as err:
+        print('error : ', err)
+    except Exception as ex:
+        print('Unexpected Error while extracting audio features', ex)
         raise
