84dd02491c8f088727a5c664b61bae487ad8abc1
Lipun Dani <lipundani@gmail.com>
2026-03-25 21:12:39 +0530

    docs: providing example config file for project setup

---
+++ backend/example_config.py
@@ -0,0 +1,27 @@
+# image train directory
+image_train_dir = r"---- provide path here ----"
+image_test_dir = r"---- provide path here ----"
+
+# audio train dir
+audio_dir = r"---- provide path of csv file here ----"
+
+# video dir for storage
+video_store_dir = r"---- provide path here ----"
+
+# audio dir for storage
+audio_store_dir = r"---- provide path here ----"
+
+# image dir for storage
+image_store_dir = r"---- provide path here ----"
+
+# saved audio model after training
+audio_model = r"---- provide audio saving model path here ----"
+
+# saved image model after training
+image_model = r"---- provide image saving model path here ----"
+
+# trained audio scalar path for model inference
+audio_scaler_path = r"---- provide trained scaler path here for inference ----"
+
+# trained label encoder path for model inference
+audio_encoder_path = r"---- provide encoder path here for inference ----"
